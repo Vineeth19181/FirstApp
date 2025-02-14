c30b1a47d5b92fbe4f82f10c9afe71666b50784c
Vineeth19181 <gvineeth253@gmail.com>
2025-02-14 08:47:58 -0600

    Test_commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Vineeth-ang' title`, () => {
+  it(`should have the 'chandu-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Vineeth-ang');
+    expect(app.title).toEqual('chandu-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Vineeth-ang');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, chandu-angular');
   });
 });
